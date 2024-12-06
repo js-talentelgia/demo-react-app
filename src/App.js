529632c0820b709da6d12893037ca9e060f04906
AnkitTLGT <ankit.chohan@talentelgia.in>
2024-12-06 18:24:36 +0530

    update text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello kubernetes argocd
+          Hello kubernetes argocd with new update
         </p>
       </header>
     </div>
